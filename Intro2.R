0bc1abfd8a25f747da0cc76d8eacc9ec0e8b01ff
Aaron A. King <kingaa@umich.edu>
2016-05-13 16:42:38 -0400

    update

--- Intro2.R
+++ Intro2.R
@@ -2,14 +2,16 @@ X <- read.csv('ChlorellaGrowth.csv',comment.char='#')
 # using '#' as a comment character allows us the CSV file to be self-documenting
 
 par(cex=1.5,cex.main=0.9)
-plot(rmax~light,data=X,xlab="light intensity (uE/m2/s)",ylab="maximum growth rate (1/d)",pch=16) 
+plot(rmax~light,data=X,xlab="light intensity (uE/m2/s)",
+     ylab="maximum growth rate (1/d)",pch=16) 
 # xlab and ylab are x and y axis labels, pch is "plotting character"
 # cex is 'character expansion' - cex=1.5 increases symbol & label sizes by 50%
 # cex.main sets the character expansion for the main title of the plot 
 
 title(main="Data from Fussmann et al. (2000) system")
 fit <- lm(rmax~Light)
-summary(fit); abline(fit) 
+summary(fit)
+abline(fit) 
 
 # Next we get the regression equation to 'display itself' on the graph
 c1 <- round(fit$coef[1],digits=3) 	# intercept	
